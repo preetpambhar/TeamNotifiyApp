fbf0b872bc11c74e24525292d9368e0511efa68a
Preet Pambhar <preetpambhar73530@gmail.com>
2024-10-29 23:40:40 -0400

    notification

---
+++ app/src/main/java/com/example/teamnotifyapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.teamnotifyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.teamnotifyapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityMainBinding mainBinding;
+
+    private static final String CHANNEL_ID = "CHANNEL_01";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+        init();
+    }
+
+    private void init() {
+        createNotificationChannel();
+        mainBinding.btnNotify.setOnClickListener(this);
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelName = getString(R.string.channel_name);
+            String channelDescription = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            channel.setDescription(channelDescription);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == mainBinding.btnNotify.getId()) {
+            String notificationTitle = getString(R.string.notification_title);
+            String notificationText = getString(R.string.notification_text);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+            builder.setContentTitle(notificationTitle);
+            builder.setContentText(notificationText);
+            builder.setSmallIcon(R.drawable.ic_notification);
+            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            managerCompat.notify(1, builder.build());
+        }
+    }
+}
---
+++ app/wearnotifyapp/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.wearnotifyapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.wearnotifyapp"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("com.google.android.gms:play-services-wearable:18.2.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.wear:wear:1.3.0")
+}
